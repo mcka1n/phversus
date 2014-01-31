41303185ce1e2c61991d8be22926f0f7f58abc6e
Ed Mckain <mckain.default@gmail.com>
2014-01-30 21:51:39 -0600

    add basic project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Phversus::Application.config.secret_key_base = 'd8876aa82b9bd1b77372e3bbce68f67ec55d4b3c1c5f43c156653cd3377af7b9cd0e88c25ee0c4839df227e79bd4de44f7b53b99ca4ed44395d5a85f226a7b33'
