7a78f53a2c25695d887834dbc2c9742e0a4a6101
Ed Mckain <mckain.default@gmail.com>
2014-02-01 00:49:39 -0600

    add a random way to show pictures from database; adds ask_for_fresh_popular_media saves data.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
 
 	def index
-		@photo = 'http://google.com/picturea'
-		@tag = 'NYC'
-		puts Instagram.user_recent_media(777)
+		@media_left = Media.all[Random.rand(Media.all.count)]
+		@media_right = Media.all[Random.rand(Media.all.count)]
+
+		@tag = @media_left.tags
+		@picture_left = @media_left.large
+		@picture_right = @media_right.large
+
 	end
 
 end
--- app/models/media.rb
+++ app/models/media.rb
@@ -1,22 +1,33 @@
 class Media < ActiveRecord::Base
 
 	def self.ask_for_fresh_popular_media
-		first = Instagram.media_popular.first
-		p '----------------- Media info ---------------------'
-		p 'type:'
-		p first.type
-		p 'tags: '
-		p first.tags
-		p "caption: "
-		p first.caption.id
-		p first.caption.text
-		p 'instagram_user_id:'
-		p first.caption.from.id
-		p first.caption.from.profile_picture
-		p first.caption.from.username
-		p '--------------------Media details--------------------'
-		p first.link
-		p first.images
+
+		popular_media = Instagram.media_popular
+		popular_media.each do |row|
+			if row.type != 'video' && row.caption.id
+				p '--------------'
+				p row
+				p '--------------'
+				does_it_exists = Media.where(:instagram_id => row.caption.id).count
+				if does_it_exists == 0
+					p '==> Boom, saving an image'
+					media = Media.new
+					media.content_type = row.type
+					media.tags = row.tags.to_s
+					media.instagram_id = row.caption.id
+					media.title = row.caption.text
+					media.instagram_user_id = row.caption.from.id
+					media.instagram_user_profile_picture = row.caption.from.profile_picture
+					media.instagram_user_username = row.caption.from.username
+					media.instagram_link = row.link
+					media.instagram_media_created_at = row.created_time
+					media.small = row.images.low_resolution.url
+					media.medium = row.images.thumbnail.url
+					media.large = row.images.standard_resolution.url
+					media.save
+				end
+			end
+		end
 	end
 
 end
