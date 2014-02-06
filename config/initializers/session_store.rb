3ceb50c0d929e4283435c3c6c5e9daf57b5059af
Ed Mckain <mckain.default@gmail.com>
2014-02-05 22:07:16 -0600

    get rid of rails 4 beta dependency, fix issue with act_as_taggable gem.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Phversus::Application.config.session_store :encrypted_cookie_store, key: '_phversus_session'
+#Phversus::Application.config.session_store :encrypted_cookie_store, key: '_phversus_session'
+
+Phversus::Application.config.session_store :cookie_store, key: '_phversus_session'
